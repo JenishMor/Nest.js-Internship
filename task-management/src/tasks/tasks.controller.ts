51434fe6d7fc116a84d77655db86f11f1302d418
jenish <jenishmor@gmail.com>
2023-05-02 16:10:26 +0530

    Adding serice for get task by id and add it in controller

--- task-management/src/tasks/tasks.controller.ts
+++ task-management/src/tasks/tasks.controller.ts
@@ -1,15 +1,22 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
 
-@Controller('tasks')
+@Controller('tasks') //This is for url (/tasks)
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
+  //   localhost:3000/tasks
   @Get() //This is Get decorator
   getAllTasks(): Task[] {
     return this.tasksService.getAllTasks();
+  }
+
+  //   localhost:3000/tasks/sdfhjkhsh
+  @Get('/:id')
+  getTaskById(@Param('id') id: string) {
+    return this.tasksService.getTaskById(id);
   }
 
   @Post() //This is Post decorator
