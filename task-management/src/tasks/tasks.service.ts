5779e72b3b60d80420917deae33382cd81451034
jenish <jenishmor@gmail.com>
2023-05-02 20:21:30 +0530

    Deleting task from database

--- task-management/src/tasks/tasks.service.ts
+++ task-management/src/tasks/tasks.service.ts
@@ -50,26 +50,17 @@ export class TasksService {
     return found;
   }
 
-  //   createTask(createTaskDto: CreateTaskDto): Task {
-  //     const { title, description } = createTaskDto;
-  //     const task: Task = {
-  //       id: uuid(),
-  //       title,
-  //       description,
-  //       status: TaskStatus.OPEN,
-  //     };
-
-  //     this.tasks.push(task);
-  //     return task;
-  //   }
   createTask(createTaskDto: CreateTaskDto): Promise<Task> {
     return this.tasksRepository.createTask(createTaskDto);
   }
 
-  //   deleteTask(id: string): void {
-  //     const found = this.getTaskById(id);
-  //     this.tasks = this.tasks.filter((task) => task.id !== found.id);
-  //   }
+  async deleteTask(id: string): Promise<void> {
+    const result = await this.tasksRepository.delete(id);
+
+    if (result.affected === 0) {
+      throw new NotFoundException(`Task with Id ${id} not found!`);
+    }
+  }
 
   //   updateTaskStatus(id: string, status: TaskStatus): Task {
   //     const task = this.getTaskById(id);
