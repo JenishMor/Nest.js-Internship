c29d53ec1b34f4de8abb7541d3318c8b53322b20
jenish <jenishmor@gmail.com>
2023-05-02 15:43:09 +0530

    Give return type to createTask handler

--- task-management/src/tasks/tasks.controller.ts
+++ task-management/src/tasks/tasks.controller.ts
@@ -11,11 +11,11 @@ export class TasksController {
     return this.tasksService.getAllTasks();
   }
 
-  @Post()
+  @Post() //This is Post decorator
   createTask(
     @Body('title') title: string,
     @Body('description') description: string,
-  ) {
+  ): Task {
     return this.tasksService.createTask(title, description);
   }
 }
