fde8af62ac285572f885c1cd648db5b301a602af
jenish <jenishmor@gmail.com>
2023-05-08 20:00:30 +0530

    Adding jwt library and set jwt strategy in auth module

--- task-management/src/auth/auth.module.ts
+++ task-management/src/auth/auth.module.ts
@@ -5,9 +5,16 @@ import { User } from './user.entity';
 import { UserRepository } from './users.repository';
 import { TypeOrmExModule } from 'src/database/typeorm-ex.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: 'ourtopsecret34',
+      signOptions: { expiresIn: 3600 },
+    }),
     TypeOrmExModule.forCustomRepository([UserRepository]),
     TypeOrmModule.forFeature([User]),
   ],
